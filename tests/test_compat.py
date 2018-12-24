edb7d8ea42ef936ffd49495393645aacf151ceb0
florimondmanca <florimond.manca@gmail.com>
2018-12-24 17:54:57 +0100

    add tests for call_async()

--- tests/test_compat.py
+++ tests/test_compat.py
@@ -1,7 +1,26 @@
 import pytest
 
-from bocadillo.compat import check_async
+from bocadillo.compat import check_async, call_async
 from bocadillo.exceptions import ShouldBeAsync
+
+
+# call_async()
+
+
+@pytest.mark.asyncio
+async def test_call_async():
+    def foo():
+        return 1
+
+    assert await call_async(foo) == 1
+
+    async def bar():
+        return 2
+
+    assert await call_async(bar) == 2
+
+
+# check_async()
 
 
 def test_if_is_async_then_ok():
